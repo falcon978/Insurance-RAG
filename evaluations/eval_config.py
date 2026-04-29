dee6b57a8fd2d4e9f7559005717ee9a1c1a8e99d
falcon <falcon978@users.noreply.github.com>
2026-04-30 00:09:23 +0530

    fix: Update default values for retrieval and reranking configurations in eval settings

--- evaluations/eval_config.py
+++ evaluations/eval_config.py
@@ -18,8 +18,8 @@ class EvalSettings(BaseSettings):
     judge_model_name: str = os.getenv("JUDGE_MODEL_NAME", "llama-3.3-70b-versatile")
 
     # Top K Retrieval and Reranking Configurations
-    retrieve_top_k: int = 20
-    rerank_top_k: int = 10
+    retrieve_top_k = int(os.getenv("retrieve_top_k", "15"))
+    rerank_top_k = int(os.getenv("rerank_top_k", "5"))
     
     # Passing Thresholds for DeepEval Standard Metrics
     recall_threshold: float = 0.8
